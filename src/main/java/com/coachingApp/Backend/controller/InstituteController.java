fa143d1259edc8010cc275316c9ebd0ab08e74b8
98SKY <coolsky300@gmail.com>
2025-04-27 15:51:57 +0530

    created webConfig andd handle Cors

--- src/main/java/com/coachingApp/Backend/controller/InstituteController.java
+++ src/main/java/com/coachingApp/Backend/controller/InstituteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/institutes")
+@RequestMapping("/api/register")
 public class InstituteController {
 
     @Autowired
